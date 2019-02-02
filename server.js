64ac37e62dd7b1841460ee4f84d23a491732edb1
Trey Eckels <trey.eckels@gmail.com>
2019-02-02 01:24:00 -0500

    Post route.

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var path = require('path');
+
 var tableData = require('./data/tableData');
 var waitingListData = require('./data/waitingListData');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'public/home.html'));
+});
+
+app.get('/tables', function(req, res) {
+	res.sendFile(path.join(__dirname, 'public/tables.html'));
+});
+
+app.get('/reserve', function(req, res) {
+	res.sendFile(path.join(__dirname, 'public/reserve.html'));
+});
 
 app.get('/api/tables', function(req, res) {
 	res.json(tableData);
@@ -11,6 +27,16 @@ app.get('/api/tables', function(req, res) {
 
 app.get('/api/waitlist', function(req, res) {
 	res.json(waitingListData);
+});
+
+app.post('/api/tables', function(req, res) {
+	if (tableData.length < 5) {
+		tableData.push(req.body);
+		res.json(true);
+	} else {
+		waitingListData.push(req.body);
+		res.json(false);
+	}
 });
 
 app.listen(PORT, function() {
