48d9e11d0561f687d5ba53b011365768c55e969b
Trey Eckels <trey.eckels@gmail.com>
2019-02-02 01:00:07 -0500

    Data and basic api routes.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
+var tableData = require('./data/tableData');
+var waitingListData = require('./data/waitingListData');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.get('/api/tables', function(req, res) {
+	res.json(tableData);
+});
+
+app.get('/api/waitlist', function(req, res) {
+	res.json(waitingListData);
+});
+
 app.listen(PORT, function() {
 	console.log('App listening on PORT ' + PORT);
 });
