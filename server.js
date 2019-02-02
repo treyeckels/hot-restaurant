675ada62f8a0bd616dbd3f741712561b4dabbc18
Trey Eckels <trey.eckels@gmail.com>
2019-02-02 00:48:38 -0500

    Basc HTML.

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log('start');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+	console.log('App listening on PORT ' + PORT);
+});
